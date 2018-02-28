789a586cd91c9c30cb988f48a927473739367da7
kristianshevando <kristianshevando@gmail.com>
2018-03-01 01:37:58 +0300

    code improvements

--- script.js
+++ script.js
@@ -41,48 +41,20 @@ var expressions = [
     }
 ];
 
-function numberOfDots(inputtedResult)
-{
-    let dotCounter = 0;
-    for(let i = 0; i < inputtedResult.length; i++) 
-    {
-        if(inputtedResult[i] == ".")
-        {
-            dotCounter++;
-        }
-    }
-    return dotCounter;
-}
-
 function mainFunction()
 {   
-    let randomOperator;
-    let firstRandomOperand;
-    let secondRandomOperand;
-    
-    let secondRandomOperandString;
-    let expectedResult;
-    let inputtedResult;
-
-    while(true) {
-        randomOperator      = Math.round(Math.random() * 3);  
-        firstRandomOperand  = Math.round(Math.random() * 20 - 10);
-        secondRandomOperand = Math.round(Math.random() * 20 - 10);
+    while(true) 
+    {
+        const randomOperator      = Math.round(Math.random() * 3);  
+        const firstRandomOperand  = Math.round(Math.random() * 20 - 10);
+        const secondRandomOperand = Math.round(Math.random() * 20 - 10);
         
-        if(secondRandomOperand < 0)
-            secondRandomOperandString = `(${secondRandomOperand})`; 
-        else
-            secondRandomOperandString = `${secondRandomOperand}`;
-    
-        expectedResult = (expressions[randomOperator].result(firstRandomOperand, secondRandomOperand)).toFixed(1);
-        console.log('Expected result: ', expectedResult);
-        console.log(firstRandomOperand);
-        console.log(secondRandomOperand);
+        const secondRandomOperandString = (secondRandomOperand < 0) ? `(${secondRandomOperand})` : `${secondRandomOperand}`;
 
-        inputtedResult = prompt(`Enter the number ${firstRandomOperand}${expressions[randomOperator].operator}${secondRandomOperandString}: `);
-        console.log(inputtedResult);
-        inputtedResult = inputtedResult.replace(",", ".");
-        console.log('Inputted result: ', inputtedResult);
+        const expectedResult = (expressions[randomOperator].result(firstRandomOperand, secondRandomOperand)).toFixed(1);
+        console.log('Expected result: ', expectedResult);
+
+        const inputtedResult = prompt(`Enter the number ${firstRandomOperand}${expressions[randomOperator].operator}${secondRandomOperandString}: `);
         
         if(+inputtedResult == +expectedResult) 
         {
@@ -90,10 +62,6 @@ function mainFunction()
         } 
         else 
         {   
-            if(numberOfDots(inputtedResult) > 1)
-            {
-                alert('Easy up man. There is too many dots :)');
-            }
             alert('I think you gotta go sleep now :)');
             break;
         }
